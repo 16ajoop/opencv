ec30e97c27045801b90a344fb78d2f89a7b10e39
poojavaithy <166294526+16ajoop@users.noreply.github.com>
2024-08-16 22:49:31 +0530

    Add files via upload

---
+++ cvprog2.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+# load a video captured ( capture as cap )
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()                 #ret tells that whether the cap is working or not
+
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()                               #freeup the webcam
+cv2.destroyAllWindows()
